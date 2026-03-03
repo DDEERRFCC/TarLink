49e090496938c5a4ddaf437b8b4fde378da3a653
wudibawanglog <woonweijie85@gmail.com>
2026-03-03 22:51:57 +0800

    Update assembly information and project paths for ITPSystem
    - Changed AssemblyInformationalVersion in ITPSystem.AssemblyInfo.cs to reflect new version.
    - Updated project directory paths in ITPSystem.GeneratedMSBuildEditorConfig.editorconfig and related files to point to the new user directory.
    - Modified project.assets.json and project.nuget.cache to align with the new project structure and paths.
    - Adjusted NuGet package paths in ITPSystem.csproj.nuget.g.props and project.nuget.cache for consistency.
    - Updated SdkAnalysisLevel in project.assets.json and project.nuget.cache to the latest version.
    - Added download dependencies for ASP.NET Core and .NET Core in project.assets.json.

--- ITPSystem/obj/Debug/net8.0/ITPSystem.AssemblyInfo.cs
+++ ITPSystem/obj/Debug/net8.0/ITPSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITPSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076dc11fe7c7d1192ebdb43e5efebc1a611d8168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670bb13121b9b529226152d79a7d4c0ac1c80dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITPSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITPSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
