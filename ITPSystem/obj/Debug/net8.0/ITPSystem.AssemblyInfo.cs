3365165a770391b7dcc2ce423f9ef7a7887c2925
chua kim chun <kimchun432@gmail.com>
2026-04-04 14:15:18 +0800

    Remove obsolete binaries and configuration files from TempVerify directory, including various Microsoft assemblies, JSON settings, and resource files. Update assembly information and source link references.

--- ITPSystem/obj/Debug/net8.0/ITPSystem.AssemblyInfo.cs
+++ ITPSystem/obj/Debug/net8.0/ITPSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITPSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d686215ce8093160f7aeb6ccc4192d9049b86cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34df6173a9a365585b6a61c07f656924c739d849")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITPSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITPSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
